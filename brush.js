37c75893c6729da207cf8c769061ddd3705bdea8
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2016-02-06 22:24:42 -0800

    Renames regex-lib to syntaxhighlighter-regex

--- brush.js
+++ brush.js
@@ -1,5 +1,5 @@
 var BrushBase = require('brush-base');
-var regexLib = require('regex-lib');
+var regexLib = require('syntaxhighlighter-regex').commonRegExp;
 
 function Brush() {
   // Contributed by Gheorghe Milas and Ahmad Sherif
